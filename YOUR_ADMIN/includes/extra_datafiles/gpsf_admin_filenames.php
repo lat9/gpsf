226c3fd80cf39503c7d5b8305e562f17a3866bb7
lat9 <lat9@vinosdefrutastropicales.com>
2026-08-20 10:54:20 -0400

    Move Merchant Center conversion to admin tool (#66)
    Ref #65

--- YOUR_ADMIN/includes/extra_datafiles/gpsf_admin_filenames.php
+++ YOUR_ADMIN/includes/extra_datafiles/gpsf_admin_filenames.php
@@ -1,9 +1,10 @@
 <?php
 // -----
 // Part of the Google Product Search Feeder II
-// Copyright 2023, https://vinosdefrutastropicales.com
+// Copyright 2023-2026, https://vinosdefrutastropicales.com
 //
-// Last updated: v1.0.0
+// Last updated: v1.1.0
 //
 define('FILENAME_GPSF_ADMIN', 'gpsf_admin');
 define('FILENAME_GPSF_MAIN_CONTROLLER', 'gpsf_main_controller');
+define('FILENAME_GPSF_EXTRAS', 'copy_google_search_configuration');
